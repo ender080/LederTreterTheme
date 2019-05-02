9dd914473b2a63e78987c5f6fb96f1e141e24633
Fidentia <43379415+Fidentia@users.noreply.github.com>
2019-05-02 16:55:51 +0200

    Update functions.php
    Editor Styles

--- functions.php
+++ functions.php
@@ -127,10 +127,11 @@ foreach ( $LederTreterTheme_includes as $file ) {
  * Enqueue editor styles for Gutenberg
  */
 function codearosa_editor_styles() {
-	wp_enqueue_style( 'codearosa-editor-style', get_stylesheet_directory_uri() . '/styles/css/custom-editor-style.min.css' );
+	wp_enqueue_style( 'codearosa-editor-style', get_stylesheet_directory_uri() . '/css/custom-editor-style.css' );
 
 }
 add_action( 'enqueue_block_editor_assets', 'codearosa_editor_styles' );
+
 
 /*colors*/
 /**
