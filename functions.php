9bbd6971b7effd23eb4a3a7819b0a65a81621fac
ender080 <4712163+ender080@users.noreply.github.com>
2020-06-29 21:10:35 +0200

    fix visual block editor, remove editor-color-palette

--- functions.php
+++ functions.php
@@ -140,24 +140,9 @@ add_action( 'enqueue_block_editor_assets', 'codearosa_editor_styles' );
 */
 function mytheme_setup_theme_supported_features() {
 
-		// Theme supports wide images, galleries and videos.
-		add_theme_support( 'align-wide' );
+	// Theme supports wide images, galleries and videos.
+	add_theme_support( 'align-wide' );
 
-		// Make specific theme colors available in the editor.
-    add_theme_support( 'editor-color-palette',
-        array(
-            'name' => 'primary',
-            'color' => '#C8689A',
-        ),
-        array(
-            'name' => 'secondary',
-            'color' => '#ffffff',
-        ),
-        array(
-            'name' => 'success',
-            'color' => '#60B7C1',
-        )
-    );
 }
 
 add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
